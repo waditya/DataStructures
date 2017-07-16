92a2a55cfd2ce86b7dc418b2ed8c2bf90d720964
waditya <waditya02@gmail.com>
2017-07-16 12:43:18 -0500

    Update Array_01.py

--- Array_01.py
+++ Array_01.py
@@ -24,6 +24,8 @@
 
 # 2 3 4 1
 
+### Code Starts
+
 #!/bin/python3
 
 import sys
@@ -31,5 +33,8 @@ import sys
 
 n = int(input().strip())
 arr = [int(arr_temp) for arr_temp in input().strip().split(' ')]
+for i in range(n-1,-1,-1): 
+    print(arr[i], end = ' ')
+
 
 
